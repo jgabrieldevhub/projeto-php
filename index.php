d920c83c7829b72928061694f5e2e0f237d5d227
Jorge Gabriel <jorgegabriel.dev@gmail.com>
2026-04-28 20:27:33 -0300

    estrutura inicial do index

--- index.php
+++ index.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Website Mulheres</title>
 </head>
 <body>
-    
+     <h4>História da programação</h4>
+    <h1>Mulheres que fizeram a diferença</h1>
+    <hr>
+    <?php
+        // aqui criar o array de mulheres
+
+    ?>
+
+    <div class="galeria">
+        
+        <?php
+        /*cada item é o conjunto de informações de uma mulher*/
+        ?>
+        <div class="item">
+            
+        </div>
+        
+    </div>
 </body>
 </html>
